f8c4408fbbf7be9efe9131f8f558c8cb49c79c84
king <king_darks@aliyun.com>
2023-05-21 19:05:35 +0800

    更新vue路由扫描

--- js/content.js
+++ js/content.js
@@ -1,1 +1,53 @@
-(function(e){function r(r){for(var n,c,l=r[0],i=r[1],a=r[2],p=0,d=[];p<l.length;p++)c=l[p],Object.prototype.hasOwnProperty.call(o,c)&&o[c]&&d.push(o[c][0]),o[c]=0;for(n in i)Object.prototype.hasOwnProperty.call(i,n)&&(e[n]=i[n]);f&&f(r);while(d.length)d.shift()();return u.push.apply(u,a||[]),t()}function t(){for(var e,r=0;r<u.length;r++){for(var t=u[r],n=!0,l=1;l<t.length;l++){var i=t[l];0!==o[i]&&(n=!1)}n&&(u.splice(r--,1),e=c(c.s=t[0]))}return e}var n={},o={content:0},u=[];function c(r){if(n[r])return n[r].exports;var t=n[r]={i:r,l:!1,exports:{}};return e[r].call(t.exports,t,t.exports,c),t.l=!0,t.exports}c.m=e,c.c=n,c.d=function(e,r,t){c.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},c.r=function(e){"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},c.t=function(e,r){if(1&r&&(e=c(e)),8&r)return e;if(4&r&&"object"===typeof e&&e&&e.__esModule)return e;var t=Object.create(null);if(c.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var n in e)c.d(t,n,function(r){return e[r]}.bind(null,n));return t},c.n=function(e){var r=e&&e.__esModule?function(){return e["default"]}:function(){return e};return c.d(r,"a",r),r},c.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},c.p="/";var l=window["webpackJsonp"]=window["webpackJsonp"]||[],i=l.push.bind(l);l.push=r,l=l.slice();for(var a=0;a<l.length;a++)r(l[a]);var f=i;u.push(["bdd7","chunk-vendors"]),t()})({bdd7:function(e,r,t){t("e260"),t("e6cf"),t("cca6"),t("a79d")}});
+/**
+ * 获取vue-router的options
+ * @auther:不懂安全的开发
+ */
+const enableDevtools = () => {
+	setChromeStorage(null)
+	const script = document.createElement('script')
+	let appId = [
+		'app',
+		'root',
+		'__nuxt',
+		'main',
+		'vue-app',
+		'app-container',
+		'app-view',
+		'app-root',
+	]
+	let checkId = ''
+	for (let i = 0; i < appId.length; i++) {
+		if (document.querySelector(`#${appId[i]}`)) {
+			checkId = appId[i]
+			break
+		}
+	}
+	if (!checkId) {
+		return
+	}
+	script.textContent = 'let options = JSON.stringify(document.querySelector("'
+	script.textContent += `#${checkId}"`
+	script.textContent += ').__vue__.$children[0].$router.options);'
+	script.textContent += 'const div = document.createElement("div");'
+	script.textContent += 'div.innerHTML = options;'
+	script.textContent += 'div.setAttribute("id", "vue-router-options");'
+	script.textContent += 'document.body.appendChild(div);'
+	document.documentElement.appendChild(script)
+	const div = document.querySelector('#vue-router-options')
+	const optionsHtml = div.innerHTML
+	if (!optionsHtml) {
+		optionsHtml = null
+	}
+	setChromeStorage(optionsHtml)
+	document.body.removeChild(div)
+	document.documentElement.removeChild(script)
+}
+const setChromeStorage = (optionsHtml) => {
+	if (optionsHtml) {
+		optionsHtml = JSON.parse(optionsHtml)
+		optionsHtml.key = document.location.host
+		optionsHtml = JSON.stringify(optionsHtml)
+	}
+	chrome.storage.sync.set({ optionsHtml }, function () {})
+}
+setTimeout(enableDevtools, 1000)
